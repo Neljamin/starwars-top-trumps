0dcab595d931f7a5011bd91399f1e27c6858def0
Aodhagan Murphy <aodhagan.murphy@magellanrobotech.com>
2020-06-09 21:35:25 +0100

    Updates

---
+++ precache-manifest.68db9d751c86466ac1b21f577f54305f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c211152d9acca323a240fa7709aba99",
+    "url": "/star-wars-top-trumps/index.html"
+  },
+  {
+    "revision": "8d225fb52c08f801ae1c",
+    "url": "/star-wars-top-trumps/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aa428dbe3b54291eb472",
+    "url": "/star-wars-top-trumps/static/js/2.8f455ea4.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/star-wars-top-trumps/static/js/2.8f455ea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d225fb52c08f801ae1c",
+    "url": "/star-wars-top-trumps/static/js/main.d3b363da.chunk.js"
+  },
+  {
+    "revision": "486cb26ea117e6c430ba",
+    "url": "/star-wars-top-trumps/static/js/runtime-main.32e3c89f.js"
+  }
+]);
